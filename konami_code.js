33dbb9fb31e6bdee38e3a78e4e345d07c0f72208
fidelscodes <fidelscodes@gmail.com>
2016-10-15 16:16:48 -0400

    Done

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,25 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 
 function init() {
-  // your code here
+  var documentBody = document.body;
+  var index = 0;
+
+  documentBody.addEventListener('keydown', onKeyDownHandler);
+
+  // unattached event handler
+  // Will serve as the second argument to addEventListener
+  function onKeyDownHandler(e) {
+    const key = parseInt(e.detail || e.which);
+
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length - 1) {
+        alert("YOU ENTERED THE SUPER SECRET KONAMI CODE!");
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  }
 }
